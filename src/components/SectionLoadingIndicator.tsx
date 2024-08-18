fb5654a5852c127b3afa61be9c395176e2dd33ff
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-08-18 11:48:45 +0300

    fix the problem that causes scrollbar shifts

--- src/components/SectionLoadingIndicator.tsx
+++ src/components/SectionLoadingIndicator.tsx
@@ -1,6 +1,10 @@
+import AnimatedSection from "./AnimatedSection";
+
 const SectionLoadingIndicator = () => {
   return (
-    <span className="loading loading-ring loading-lg size-24 bg-gradient-to-r from-[#3bb5e6] to-[#fd47b4]"></span>
+    <AnimatedSection sectionId="section-loading-indicator">
+      <span className="loading loading-ring loading-lg size-24 bg-gradient-to-r from-[#3bb5e6] to-[#fd47b4]"></span>
+    </AnimatedSection>
   );
 };
 export default SectionLoadingIndicator;
