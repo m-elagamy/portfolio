1510b9340bd8aaf6662b7a8c7b36a1958781e695
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-08-31 14:24:19 +0300

    change the position to fixed

--- src/components/sections/Header/ScrollIndicator.tsx
+++ src/components/sections/Header/ScrollIndicator.tsx
@@ -1,13 +1,11 @@
 const ScrollIndicator = ({ scrollProgress }: { scrollProgress: number }) => {
   return (
-    <div className="relative">
-      <div
-        className="absolute left-0 top-0 h-1 rounded bg-[#3bb5e6]"
-        style={{
-          width: `${scrollProgress}%`,
-        }}
-      />
-    </div>
+    <div
+      className="fixed left-0 top-0 z-[51] h-[3px] rounded bg-[#3bb5e6]"
+      style={{
+        width: `${scrollProgress}%`,
+      }}
+    />
   );
 };
 export default ScrollIndicator;
